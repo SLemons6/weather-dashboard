fcea08e89f27638741d017b3e8721dee80526e09
Shayne Lemons <lemonsshayne6@gmail.com>
2022-01-10 13:06:03 -0800

    complete script.js, pending bug tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Globals
 var citySearchHistory = [];
 //  OpenWeather API variables
-var weatherURl = 'https://api.openweathermap.org';
-var APIKey = 'a0aa59ab54c8737ca6fbb9392215f40f';
+var weatherUrl = 'https://api.openweathermap.org';
+var apiKey = 'a0aa59ab54c8737ca6fbb9392215f40f';
 
 // DOM elements
 var cityForm = document.querySelector('#city-form');
-var citySearch = document.querySelector('#city-search');
+var citySearchInput = document.querySelector('#city-input');
 var searchHistoryEl = document.querySelector('#search-history');
 var currentWeatherEl = document.querySelector('#current-weather');
 var futureWeatherEl = document.querySelector('#future-weather');
@@ -20,13 +20,13 @@ function showSearchHistory() {
     searchHistoryEl.innerHTML = '';
 
     // display search history from most recent to least recent 
-    for (var i = citySearchHistory.length - 1; i > 0; i--) {
+    for (var i = citySearchHistory.length - 1; i >= 0; i--) {
         var btn = document.createElement('button');
         btn.setAttribute('type', 'button');
-        btn.setAttribute('aria-controls', 'current-weather future-weather');
-        btn.classList.add('search-history-btn');
-        btn.setAttribute('citySearchHistory');
-        btn.textContent = citySearchHistory[1];
+        btn.setAttribute('current', 'aria-controls');
+        btn.classList.add('btn', 'btn-secondary', 'search-history-btn');
+        btn.setAttribute('city-search', citySearchHistory[i]);
+        btn.textContent = citySearchHistory[i];
         searchHistoryEl.append(btn);
     }
 }
@@ -53,10 +53,10 @@ function fetchHistory() {
 }
 
 // Display current weather
-function showCurrentWeather(city, weather, time) {
+function showCurrent(city, weather, time) {
     var currentDate = dayjs().tz(time).format('M/D/YYYY');
 
-    // Store response data from our fetch request in variables
+    // Store data from Open Weather API fetch request
     var temperature = weather.temp;
     var windSpeed = weather.wind_speed;
     var humidity = weather.humidity;
@@ -76,7 +76,7 @@ function showCurrentWeather(city, weather, time) {
 
     weatherCard.setAttribute('class', 'card');
     weatherCardBody.setAttribute('class', 'card-body');
-    WeatherCard.append(weatherCardBody);
+    weatherCard.append(weatherCardBody);
 
     heading.setAttribute('class', 'h3 card-title');
     temperatureEl.setAttribute('class', 'card-text');
@@ -89,9 +89,9 @@ function showCurrentWeather(city, weather, time) {
     weatherIcon.setAttribute('class', 'weather-img');
     heading.append(weatherIcon);
     temperatureEl.textContent = `Temp: ${temperature}°F`;
-    windEl.textContent = `Wind: ${windSpeed} MPH`;
+    windSpeedEl.textContent = `Wind: ${windSpeed} MPH`;
     humidityEl.textContent = `Humidity: ${humidity} %`;
-    weatherCardBody.append(heading, tempEl, windEl, humidityEl);
+    weatherCardBody.append(heading, temperatureEl, windSpeedEl, humidityEl);
 
     uviEl.textContent = 'UV Index: ';
     uviButton.classList.add('btn', 'btn-sm');
@@ -105,12 +105,149 @@ function showCurrentWeather(city, weather, time) {
     }
 
     uviButton.textContent = uvi;
-    uviEl.append(uviBadge);
-    WeatherCardBody.append(uvEl);
+    uviEl.append(uviButton);
+    weatherCardBody.append(uviEl);
 
     currentWeatherEl.innerHTML = '';
     currentWeatherEl.append(weatherCard);
 }
 
+
+// Display a forecast card
+function createForecastCard(forecast, time) {
+    // variables for data from api
+    var unixtext = forecast.dt;
+    var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+    var iconDescription = forecast.weather[0].description;
+    var tempF = forecast.temp.day;
+    var { humidity } = forecast;
+    var windSpeed = forecast.wind_speed;
+  
+    // Create elements for a card
+    var column = document.createElement('div');
+    var forecastCard = document.createElement('div');
+    var forecastCardBody = document.createElement('div');
+    var cardTitle = document.createElement('h5');
+    var weatherIcon = document.createElement('img');
+    var temperatureEl = document.createElement('p');
+    var windSpeedEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+  
+    column.append(forecastCard);
+    card.append(forecastCardBody);
+    forecastCardBody.append(cardTitle, weatherIcon, temperatureEl, windSpeedEl, humidityEl);
+  
+    column.setAttribute('class', 'col-md');
+    forecastCard.setAttribute('class', 'card bg-primary h-100 text-white');
+    forecastCardBody.setAttribute('class', 'card-body p-2');
+    forecastCardTitle.setAttribute('class', 'card-title');
+    temperatureEl.setAttribute('class', 'card-text');
+    windSpeedEl.setAttribute('class', 'card-text');
+    humidityEl.setAttribute('class', 'card-text');
+  
+    // Update elements 
+    forecastCardTitle.textContent = dayjs.unix(unixtext).tz(time).format('M/D/YYYY');
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconDescription);
+    temperatureEl.textContent = `Temp: ${tempF} °F`;
+    windSpeedEl.textContent = `Wind: ${windSpeed} MPH`;
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+  
+    futureWeatherEl.append(column);
+  }
+  
+  // Display 5 day forecast.
+  function displayFiveDayForecast(forecast, time) {
+    // Create unix timestamps for forecast
+    var firstDay = dayjs().tz(time).add(1, 'day').startOf('day').unix();
+    var lastDay = dayjs().tz(time).add(6, 'day').startOf('day').unix();
+  
+    var forecastColumn = document.createElement('div');
+    var forecastHeading = document.createElement('h4');
+  
+    forecastColumn.setAttribute('class', 'col-12');
+    forecastHeading.textContent = '5-Day Forecast:';
+    forecastColumn.append(forecastHeading);
+  
+    futureWeatherEl.innerHTML = '';
+    futureWeatherEl.append(forecastColumn);
+    for (var i = 0; i < forecast.length; i++) {
+      if (forecast[i].dt >= firstDay && forecast[i].dt < lastDay) {
+        displayFiveDayForecast(forecast[i], time);
+      }
+    }
+  }
+  
+  function renderItems(city, data) {
+    showCurrent(city, data.current, data.time);
+    displayFiveDayForecast(data.daily, data.time);
+  }
+  
+  // Find weather based on latitude and longitude coordinates, and call functions to display forecast
+  function findWeather(location) {
+    var { lat } = location;
+    var { lon } = location;
+    var city = location.name;
+    var oWUrl = `${weatherUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
+  
+    fetch(oWUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        renderItems(city, data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+  
+  function findCoordinates(search) {
+    var oWUrl = `${weatherUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${apiKey}`;
+  
+    fetch(oWUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        if (!data[0]) {
+          alert('Location not found');
+        } else {
+          updateHistory(search);
+          findWeather(data[0]);
+        }
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+  
+  function searchSubmit(event) {
+    // Don't continue if there is nothing in the search form
+    if (!citySearchInput.value) {
+      return;
+    }
+  
+    event.preventDefault();
+    var search = citySearchInput.value.trim();
+    findCoordinates(search);
+    citySearchInput.value = '';
+  }
+  
+  function searchHistoryClick(event) {
+    // Don't do search if current elements is not a search history button
+    if (!event.target.matches('.search-history-btn')) {
+      return;
+    }
+  
+    var btn = event.target;
+    var search = btn.getAttribute('city-search');
+    findCoordinates(search);
+  }
+  
+  fetchHistory();
+  cityForm.addEventListener('submit', searchSubmit);
+  searchHistoryEl.addEventListener('click', searchHistoryClick);
+  
 
 
